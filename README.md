8a472b4698c05d1a3d649740ddf6a633f0915d8a
Jonas Arnfred <ja@gower.st>
2020-08-31 20:34:34 +0100

    Basic HTTP hello world based on Cowboy
    This code is adapted from
    https://gist.github.com/flbuddymooreiv/ce1d7a47b12c27bf1616 but uses a
    recent version of Cowboy.

---
+++ README.md
@@ -0,0 +1,9 @@
+kind-rest
+=====
+
+Rest interface for the kind language. Useful for an online demo.
+
+Build and run
+-----
+
+    $ rebar3 run
---
+++ apps/kind_rest/src/kind_rest_app.erl
@@ -0,0 +1,20 @@
+-module(kind_rest_app).
+-export([start/2]).
+
+start(_StartType, _StartArgs) ->
+    {ok, Pid} = 'kind_rest_sup':start_link(),
+    Routes = [ {
+        '_',
+        [
+            {"/", kind_rest_root, []}
+        ]
+    } ],
+    Dispatch = cowboy_router:compile(Routes),
+
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    ProtoOpts = #{env => #{dispatch => Dispatch}},
+
+    {ok, _} = cowboy:start_clear(kind_rest_test,
+        TransOpts, ProtoOpts),
+
+    {ok, Pid}.
