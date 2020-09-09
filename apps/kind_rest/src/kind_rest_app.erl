d3cee2cc4bcd1345e6f136e6c90d290ac6a177e1
Jonas Arnfred <ja@gower.st>
2020-09-09 20:17:11 +0100

    Working static landing page with not very useful information
    I've copied the landing page from www.dynkarken.com and set up `cowboy`
    to serve the static files. Next step is to change it to something more
    appropriate for a programming language than a caramel company.

--- apps/kind_rest/src/kind_rest_app.erl
+++ apps/kind_rest/src/kind_rest_app.erl
@@ -1,20 +1,22 @@
 -module(kind_rest_app).
 -export([start/2]).
 
--define(PORT, os:getenv("PORT", 2938)).
+-define(PORT, list_to_integer(os:getenv("PORT", "2938"))).
 
 start(_StartType, _StartArgs) ->
     {ok, Pid} = 'kind_rest_sup':start_link(),
     Routes = [ {
         '_',
         [
-            {"/", kind_rest_root, []},
-	    {"/run", kind_rest_run, []}
+	    %{"/", kind_rest_root, []},
+	    {"/", cowboy_static, {priv_file, kind_rest, "static/index.html"}},
+	    {"/run", kind_rest_run, []},
+	    {"/static/[...]", cowboy_static, {priv_dir, kind_rest, "static"}}
         ]
     } ],
     Dispatch = cowboy_router:compile(Routes),
 
-    TransOpts = [ {ip, {0,0,0,0}}, {port, list_to_integer(?PORT)} ],
+    TransOpts = [ {ip, {0,0,0,0}}, {port, ?PORT} ],
     ProtoOpts = #{env => #{dispatch => Dispatch}},
 
     {ok, _} = cowboy:start_clear(kind_rest_test,
